5439116e31cf53808ae2d1569eac6f064737517c
Diego Andres Ruiz Duarte <diegoandres.ruiz@globant.com>
2021-09-09 17:13:01 -0500

    element

--- src/Components/Slate/SlateTest.js
+++ src/Components/Slate/SlateTest.js
@@ -18,34 +18,13 @@ function RichTextEditor(props) {
 
   const renderElement = useCallback(props => <Element {...props} />, [])
 
-  const Element = ({ attributes, children, element }) => {
-    switch (element.type) {
-      case 'block-quote':
-        return <blockquote {...attributes}>{children}</blockquote>
-      case 'bulleted-list':
-        return <ul {...attributes}>{children}</ul>
-      case 'code':
-        return (
-          <pre {...attributes}>
-            <code>{children}</code>
-          </pre>
-        )
-      case 'heading-one':
-        return <h1 {...attributes}>{children}</h1>
-      case 'heading-two':
-        return <h2 {...attributes}>{children}</h2>
-      case 'list-item':
-        return <li {...attributes}>{children}</li>
-      case 'numbered-list':
-        return <ol {...attributes}>{children}</ol>
-      default:
-        return <p {...attributes}>{children}</p>
-    }
-  }
+
 
   const renderLeaf = useCallback(props => {
     return <Leaf {...props} />
   }, [])
+
+
 
   return (
     // Add a toolbar with buttons that call the same methods.
@@ -116,6 +95,53 @@ function RichTextEditor(props) {
 
 
 export default RichTextEditor;
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case 'block-quote':
+      return <blockquote {...attributes}>{children}</blockquote>
+    case 'bulleted-list':
+      return <ul {...attributes}>{children}</ul>
+    case 'code':
+      return (
+        <pre {...attributes}>
+          <code>{children}</code>
+        </pre>
+      )
+    case 'heading-one':
+      return <h1 {...attributes}>{children}</h1>
+    case 'heading-two':
+      return <h2 {...attributes}>{children}</h2>
+    case 'list-item':
+      return <li {...attributes}>{children}</li>
+    case 'numbered-list':
+      return <ol {...attributes}>{children}</ol>
+    default:
+      return <p {...attributes}>{children}</p>
+  }
+}
+
+// Define a React component to render leaves with bold text.
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>
+  }
+
+  return <span {...attributes}>{children}</span>
+}
+
 
 const CustomEditor = {
   isBoldMarkActive(editor) {
@@ -189,23 +215,3 @@ const CustomEditor = {
   },
 }
 
-// Define a React component to render leaves with bold text.
-const Leaf = ({ attributes, children, leaf }) => {
-  if (leaf.bold) {
-    children = <strong>{children}</strong>
-  }
-
-  if (leaf.code) {
-    children = <code>{children}</code>
-  }
-
-  if (leaf.italic) {
-    children = <em>{children}</em>
-  }
-
-  if (leaf.underline) {
-    children = <u>{children}</u>
-  }
-
-  return <span {...attributes}>{children}</span>
-}
