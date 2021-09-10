2c163892a566cc2b051d40bb444ffb58241a72b0
Diego Andres Ruiz Duarte <diegoandres.ruiz@globant.com>
2021-09-10 11:33:20 -0500

    elements styles

--- src/Components/Slate/RichTextEditor.js
+++ src/Components/Slate/RichTextEditor.js
@@ -13,12 +13,7 @@ function RichTextEditor(props) {
 
   const [editor] = useState(withHistory(withReact(createEditor())), [])
   //const editor = useMemo(() => withReact(createEditor()), [])
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ])
+  const [value, setValue] = useState(initialValue);
 
   const renderElement = useCallback(props => <Element {...props} />, []);
 
@@ -177,4 +172,48 @@ const CustomEditor = {
     )
   },
 }
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [
+      { text: 'This is editable ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' },
+    ],
+  },
+  {
+    type: 'h2',
+    children: [
+      { text: 'Titulo' },
+
+    ],
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text:
+          "Since it's rich text, you can do things like turn a selection of text ",
+      },
+      { text: 'bold', bold: true },
+      {
+        text:
+          ', or add a semantically rendered block quote in the middle of the page, like this:',
+      },
+    ],
+  },
+  {
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }],
+  },
+  {
+    type: 'paragraph',
+    children: [{ text: 'Try it out for yourself!' }],
+  },
+]
 
